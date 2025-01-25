8151bbf82a0ab6d670f3964f19ea7a7ddb84aeeb
piero rossoni <64088635+pierorossoni@users.noreply.github.com>
2025-01-25 17:07:14 -0300

    Initial commit from your app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1>Embedded Webpage</h1>
+    <div style={{ margin: '10px' }}>
+      <h1>Agenda</h1>
       <iframe
         src="https://typebot.co/lembra-piero"
-        style={{ border: 'none', width: '100%', height: '600px' }}
+        style={{ border: 'none', width: '100%', height: 'calc(100vh - 20px)' }}
       ></iframe>
     </div>
   );
